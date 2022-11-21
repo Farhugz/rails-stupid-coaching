22386e2c4561e9848de479b429f1baaea6e6f3c7
Hugo Farrugia <hugofarrugia@gmail.com>
2022-11-21 16:54:56 +0100

    Finally finished

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,16 @@
-QUESTION_WORK = "I am going to work"
-ANSWER_WORK = "Great!"
-ANSWER_QUESTION = "Silly question, get dressed and go to work!"
-ANSWER_ELSE = "I don't care, get dressed and go to work!"
-
 class QuestionsController < ApplicationController
-    def ask
-    end
+  def ask
+  end
 
-    def answer
-        @question = params[:question]
-        @answer = stupid_coach(@question)
+  def answer
+    @question = ''
+    @question = params[:question] if params[:question]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
     end
-
-    private
-
-    def stupid_coach(str)
-        is_question?(str) ? ANSWER_QUESTION : ((str === QUESTION_WORK) ? ANSWER_WORK : ANSWER_ELSE)
-    end
-
-    def is_question?(str)
-        (str === nil) ? false : str.end_with?("?")
-    end
+  end
 end
